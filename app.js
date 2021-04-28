930580f7407af31a7d0e8c8d214976f1c2a7578b
Sofia Tejada <sofianais@icloud.com>
2021-04-28 15:40:28 -0700

    add more buttons to the name tag

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mediumPurple = document.getElementById("medium-purple");
 const powderBlue = document.getElementById("powder-blue");
 const background = document.getElementById("background");
 const message = document.getElementById("message");
+const stuff = document.getElementById("stuff")
 
 //get some listeners
 button.addEventListener('click', () => {
@@ -18,11 +19,31 @@ button.addEventListener('click', () => {
 //get listeners for color buttons
 lavenderBlush.addEventListener('click', () => {
     background.style.background = '#FFF0F5';
-    message.style.color = 'black';
+    message.style.color = '#BA55D3';
+    stuff.style.color = '#BA55D3';
 })
 powderBlue.addEventListener('click', () => {
     background.style.background = '#B0E0E6';
+    message.style.color = '#191970';
+    stuff.style.color = '#191970';
 })
 mediumPurple.addEventListener('click', () => {
     background.style.background = '#9370DB';
+    message.style.color = 'white';
+    stuff.style.color = 'white';
+})
+
+//pronoun get elements by ids
+const fem = document.getElementById("fem");
+const masc = document.getElementById("masc");
+const non = document.getElementById("non");
+const pronoun = document.getElementById("pronoun");
+const select = document.getElementById("select");
+const add = document.getElementById("add");
+
+//event listeners
+pronoun.addEventListener('click', () => {
+    
+    //add.textContent = select.value;
+    userName.textContent = userInput.value + select.value;
 })
